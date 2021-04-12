05e058d2fb9d471e892c1287944187ba4272c580
leitongchen <leitongchen93@gmail.com>
2021-04-12 14:28:56 +0200

    esercizio risolto

---
+++ myscript.js
@@ -0,0 +1,34 @@
+// array che contiene i valori
+var allNumbers = [];
+
+var initialNumber = 1
+
+// ciclo che stampa i numeri fino a 100
+for (var i = 1; i <= 100; i++) {
+
+    //variabile per i numeri multipli di 3 e di 5
+    /* var fizz = i % 3 === 0;
+    var buzz = i % 5 === 0; */ 
+
+    var numeri = i;
+
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        numeri = "fizzbuzz";
+
+    } else if (i % 5 === 0) {
+        numeri = "buzz";
+
+    } else if (i % 3 === 0) {
+        numeri = "fizz";
+
+    }
+
+    allNumbers.push(numeri);
+    //i valori ottenuti vengono pushati nell'array allNumbers
+
+}
+
+    // stampare allNumbers che contiene sia num che fizz e buzz al posto dei multipli di 3 e 5
+
+    console.log(allNumbers.join(" - "));
